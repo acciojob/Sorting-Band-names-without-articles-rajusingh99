d14fb96c12d68b31c86bc3136beacf4ca0a556e2
Raju Singh <94919614+rajusingh99@users.noreply.github.com>
2023-07-08 13:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-//your code here
-// alert(9)
+ let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'The Rolling Stones', 'Metallica'];
 
-let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'The Rolling Stones', 'Metallica'];
+    // Function to remove articles from the beginning of band names
+    function removeArticles(name) {
+      return name.replace(/^(a|an|the)\s+/i, '');
+    }
 
-// Function to remove articles from the beginning of band names
-function removeArticles(name) {
-  return name.replace(/^(a|an|the)\s+/i, '');
-}
+    // Sort the band names in lexicographic order (excluding articles)
+    bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
 
-// Sort the band names in lexicographic order (excluding articles)
-bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
-
-// Create the list and add list items
-const ul = document.getElementById('band');
-bandNames.forEach(name => {
-  const li = document.createElement('li');
-  li.textContent = name;
-  ul.appendChild(li);
-});
+    // Create the list and add list items
+    const ul = document.getElementById('band');
+    bandNames.forEach(name => {
+      const li = document.createElement('li');
+      li.textContent = name;
+      ul.appendChild(li);
+    });
